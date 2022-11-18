6b0b0ef8e10be619be4df86b95f006963a197824
Илья Гребнев <grebnevilia@outlook.com>
2022-11-18 09:58:00 +0700

    Листинг 5.7. PrimeGenerator.cs (окончательная версия)

--- 18.11/PrimeGenerator.cs
+++ 18.11/PrimeGenerator.cs
@@ -11,6 +11,25 @@ public class PrimeGenerator
 {
     private static bool[] isCrossed;
     private static int[] result;
+    private static void PutUncrossedIntegersIntoResult()
+    {
+        result = new int[NumberOfUncrossedIntegers()];
+        for (int j = 0, i = 2; i < isCrossed.Length; i++)
+        {
+            if (NotCrossed(i))
+                result[j++] = i;
+        }
+    }
+    private static int NumberOfUncrossedIntegers()
+    {
+        int count = 0;
+        for (int i = 2; i < isCrossed.Length; i++)
+        {
+            if (NotCrossed(i))
+                count++; // увеличить счетчик
+        }
+        return count;
+    }
     public static int[] GeneratePrimeNumbers(int maxValue)
     {
         if (maxValue < 2)
